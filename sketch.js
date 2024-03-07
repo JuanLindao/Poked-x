1817525319482842a89b1f9b1c2c6b915c71651f
Estudante Alura <106817261+JuanLindao@users.noreply.github.com>
2024-03-07 08:28:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  background(220);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(220);
+}
